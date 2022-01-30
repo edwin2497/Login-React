159b6a3a5dda854a8cd0731a3cef502761a6443a
Edwin León <edwin1224leon@gmail.com>
2022-01-30 11:08:43 -0600

    Login React Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  /*const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };*/
+
+  const handleEmailChange = ({ target: { value } }) => setEmail(value);
+
+  /*const handlePasswordChange = (event) => {  
+    setPassword(event.target.value);
+  };*/
+
+  const handlePasswordChange = ({ target: { value } }) => setPassword(value);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault(); //Prevent page refresh after button press
+    alert(`User: ${email}, Password: ${password}`)//Send alert with the values
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleFormSubmit}>
+        <h2>Log In</h2>
+        <label>
+          Email
+          <input type="email" value={email} onChange={handleEmailChange}/>
+        </label>
+        <label>
+          Password
+          <input type="password" value={password} onChange={handlePasswordChange}/>
+        </label>
+        <button type ="submit">Enter</button>
+      </form>
     </div>
   );
 }
